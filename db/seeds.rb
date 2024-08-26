2746f17d03180f426669edb4190d865cf0810c69
Guilherme Rizzo <grsp+lewagon73dd8znudr@pm.me>
2024-08-26 15:40:02 -0300

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Castle.create(name: "Neuschwanstein", location: "Germany", castle_picture: "https://i.natgeofe.com/n/630e4e81-c99c-459d-afce-249615d9f268/neuschwanstein-castle-bavaria-germany.jpg")
+Castle.create(name: "Bran Castle", location: "Transylvania, Romania", castle_picture: "https://i.natgeofe.com/n/84e54901-e481-4375-87c0-b546ebe9e87b/bran-castle-romania.jpg")
+Castle.create(name: "Chambord Castle", location: "Frace", castle_picture: "https://i.natgeofe.com/n/d05f2683-2788-4611-a795-e465f7f7e496/chambord-chateau-france.jpg")
+Castle.create(name: "Moszna Castle", location: "Poland", castle_picture: "https://i.natgeofe.com/n/64665f1b-44ba-4247-ad36-3ff577e9a9d0/moszna-castle-poland.jpg")
+Castle.create(name: "Leeds Castle", location: "Kent, England", castle_picture: "https://i.natgeofe.com/n/0981af44-6ea7-4ccd-821a-ce8ef0d484ed/leeds-castle-kent-united-kingdom.jpg")
+Castle.create(name: "St. Vitus Cathedral Prague Castle", location: "Czech Republic", castle_picture: "https://i.natgeofe.com/n/f09538db-f192-4edc-8425-46890a931d8e/st-vitus-cathedral-prague-castle-czech-republic.jpg")
+Castle.create(name: "Pena Palace", location: "Sintra, Portugal", castle_picture: "https://i.natgeofe.com/n/35c41224-1d08-4aa0-bb3e-e5bc66f62ed8/pena-palace-sintra-portugal.jpg")
+Castle.create(name: "Bojnice Castle", location: "Slovakia", castle_picture: "https://i.natgeofe.com/n/0a04e8fd-418c-4525-8722-6fde0ba2a181/bojnice-castle-slovakia.jpg")
+Castle.create(name: "Segovia Fortress", location: "Spain", castle_picture: "https://i.natgeofe.com/n/2341a20c-a5d8-4133-bfce-aa1e4c8a2ffa/segovia-fortress-spain.jpg")
+Castle.create(name: "Buda Castle Budapest", location: "Budapest, Hungary", castle_picture: "https://i.natgeofe.com/n/c4d0df85-ca3b-4cc3-93a3-e0f418f3619b/buda-castle-budapest-hungary.jpg")
