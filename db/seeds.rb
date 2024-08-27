3290639be604c02bbd09904db7dc222194604d10
Thomas Wasson <gtwasson@gmail.com>
2024-08-27 16:29:57 -0400

    seedfile update and index links

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ Castle.create!(
   availability: false,
   user: cansada
 )
-Castle.create(
+Castle.create!(
   castle_name: "Bran Castle",
   location: "Bran, Transylvania",
   castle_picture: "https://i.natgeofe.com/n/84e54901-e481-4375-87c0-b546ebe9e87b/bran-castle-romania.jpg",
@@ -31,7 +31,7 @@ Castle.create(
   availability: false,
   user: cansada
   )
-Castle.create(
+Castle.create!(
   castle_name: "Chambord Castle",
   location: "Val de Loire, France",
   castle_picture: "https://i.natgeofe.com/n/d05f2683-2788-4611-a795-e465f7f7e496/chambord-chateau-france.jpg",
@@ -41,7 +41,7 @@ Castle.create(
   availability: false,
   user: cansada
 )
-Castle.create(
+Castle.create!(
   castle_name: "Moszna Castle",
   location: "Mozna, Poland",
   castle_picture: "https://i.natgeofe.com/n/64665f1b-44ba-4247-ad36-3ff577e9a9d0/moszna-castle-poland.jpg",
@@ -51,7 +51,7 @@ Castle.create(
   availability: false,
   user: cansada
   )
-Castle.create(
+Castle.create!(
   castle_name: "Leeds Castle",
   location: "Kent, England",
   castle_picture: "https://i.natgeofe.com/n/0981af44-6ea7-4ccd-821a-ce8ef0d484ed/leeds-castle-kent-united-kingdom.jpg",
@@ -61,7 +61,7 @@ Castle.create(
   availability: false,
   user: cansada
   )
-Castle.create(
+Castle.create!(
   castle_name: "St. Vitus Cathedral",
   location: "Prague, Czech Republic",
   castle_picture: "https://i.natgeofe.com/n/f09538db-f192-4edc-8425-46890a931d8e/st-vitus-cathedral-prague-castle-czech-republic.jpg",
@@ -71,7 +71,7 @@ Castle.create(
   availability: false,
   user: cansada
   )
-Castle.create(
+Castle.create!(
   castle_name: "Pena Palace",
   location: "Sintra, Portugal",
   castle_picture: "https://i.natgeofe.com/n/35c41224-1d08-4aa0-bb3e-e5bc66f62ed8/pena-palace-sintra-portugal.jpg",
@@ -81,7 +81,7 @@ Castle.create(
   availability: false,
   user: cansada
   )
-Castle.create(
+Castle.create!(
   castle_name: "Bojnice Castle",
   location: "Bojnice, Slovakia",
   castle_picture: "https://i.natgeofe.com/n/0a04e8fd-418c-4525-8722-6fde0ba2a181/bojnice-castle-slovakia.jpg",
@@ -91,7 +91,7 @@ Castle.create(
   availability: false,
   user: cansada
   )
-Castle.create(
+Castle.create!(
   castle_name: "Alcazar of Segovia",
   location: "Segovia, Spain",
   castle_picture: "https://i.natgeofe.com/n/2341a20c-a5d8-4133-bfce-aa1e4c8a2ffa/segovia-fortress-spain.jpg",
@@ -101,7 +101,7 @@ Castle.create(
   availability: false,
   user: cansada
   )
-Castle.create(
+Castle.create!(
   castle_name: "Buda Castle",
   location: "Budapest, Hungary",
   castle_picture: "https://i.natgeofe.com/n/c4d0df85-ca3b-4cc3-93a3-e0f418f3619b/buda-castle-budapest-hungary.jpg",
