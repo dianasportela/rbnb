1bf166fd87e8a476297057b1419f152930606bb3
jasmim mattos <jasmimfm@gmail.com>
2024-08-27 11:57:30 -0300

    adding validations to Castle + castle params

--- app/controllers/castles_controller.rb
+++ app/controllers/castles_controller.rb
@@ -8,31 +8,31 @@ class CastlesController < ApplicationController
   def new
     @castle = Castle.new
   end
-  
+
   def edit
   end
-  
+
   def update
     if @castle.update(castle_params)
       redirect_to @castle, notice: "Castle was successfully updated.", status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
-  end  
-  
+  end
+
   def destroy
     @castle.destroy
-    
+
     redirect_to castles_path, notice: "Castle was successfully destroyed.", status: :see_other
   end
-  
+
   def show
   end
-  
+
   private
 
   def castle_params
-    params.require(:castle).permit(:overview)
+    params.require(:castle).permit(:castle_name, :overview, :location, :castle_picture)
   end
 
   def set_castle
