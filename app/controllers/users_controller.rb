ad8ef32ab03bb82736ae700b018bda339b6231ef
Guilherme Rizzo <grsp+lewagon73dd8znudr@pm.me>
2024-08-30 10:21:39 -0300

    temp change - pending refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,11 @@ class UsersController < ApplicationController
 
   # depreciated method - moved to 'bookings_controller'
   def show
+    @bookings = current_user.bookings
+    @castle = Castle.new
     @my_bookings = current_user.bookings
     @my_castles = current_user.castles
-    @bookings = @castle.bookings.all
+    # @bookings = @castle.bookings.all
   end
 
   private
