2495f8a8adc727b2c69ab544202ce31aaeccdfc1
jasmim mattos <jasmimfm@gmail.com>
2024-08-30 11:33:08 -0300

    improving the new castle form  and the new booking form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: :show
-  before_action :set_castle, only: :show
+  
 
   def show
     @bookings = current_user.bookings
--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,13 +2,27 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["togglableElement"]
+  static targets = ['btn', 'form', 'errors']
 
   connect() {
-    console.log("connected controller togle");
+    // console.log(this.btnTarget);
+    // console.log(this.formTarget);
+    // console.log(this.errorsTarget);
+    this.checkForErrors();
   }
 
-  fire() {
-    this.togglableElementTarget.classList.toggle("d-none");
+  openForm() {
+    this.formTarget.classList.remove("d-none");
   }
+
+  closeForm() {
+    this.formTarget.classList.add('d-none');
+  }
+
+  checkForErrors() {
+    if (this.errorsTarget.innerHTML.trim() !== "") {
+      this.openForm();
+    }
+  }
+
 }
